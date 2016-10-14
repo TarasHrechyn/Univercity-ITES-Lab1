045e8d15cbe07dc817303181266aae8a7616b54b
CP2047\Taras <taras.hrechyn@hotmail.com>
2016-10-14 14:38:21 +0300

    Forms Added

---
+++ ITES_Labs/WpfPowerCalcApp/MainAppClass.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace ConsolePowerUsageCalcVisual
+{
+    [Serializable]
+    public class PowerStation
+    {
+        // список обєктів
+        [XmlArray]
+        public List<PowerItem> Items = new List<PowerItem>();
+       
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        public PowerStation()
+        {            
+        }
+        /// <summary>
+        /// ввід з файла XML
+        /// </summary>
+        public static PowerStation LoadFromXML(string aFileName)
+        {
+            XmlSerializer xml = new XmlSerializer(typeof(PowerStation));
+            try
+            {
+                using (Stream stream = new FileStream(aFileName, FileMode.Open, FileAccess.Read, FileShare.Read))
+                {
+                    return (PowerStation)xml.Deserialize(stream);
+                }
+            }
+            // опрацювання помилок
+            catch 
+            {
+                return new PowerStation();
+//                Console.WriteLine("Invalid document content  {0}: {1}", aFileName, e.Message);
+//                Console.ReadKey();
+            }
+
+        }
+
+        /// <summary>
+        /// ввід з файла XML
+        /// </summary>
+        public void SaveToXML(string aFileName)
+        {
+            XmlSerializer xml = new XmlSerializer(typeof(PowerStation));
+            using (Stream stream = new FileStream(aFileName, FileMode.Create, FileAccess.Write, FileShare.None))
+            {
+                xml.Serialize(stream, this);
+            } 
+        }
+
+        /// <summary>
+        /// ввивід результату
+        /// </summary>
+        public Complex GetSum()
+        {
+            Complex Ssum = Complex.Zero;
+
+            foreach (PowerItem item in Items)
+            {
+                Ssum += item.Snom;
+            }
+            return Ssum;
+        }
+
+        /// <summary>
+        /// Приклад Запиту
+        /// </summary>
+        /// <param name="aVoltage"> параметри запиту </param>
+        internal List<PowerItem> ItemsByVoltage(double aVoltage)
+        {
+            Console.WriteLine("Items at voltage {0}", aVoltage);
+            var subItems = from item in Items
+                           where ((item.Vnom == aVoltage) || (aVoltage == 0.0))
+                           select item;
+            return new List<PowerItem>(subItems);
+        }
+    }
+}
---
+++ ITES_Labs/WpfPowerCalcApp/MainWindow.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ConsolePowerUsageCalcVisual;
+
+namespace WpfPowerCalcApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //        private PowerStation stationModel = new PowerStation();
+
+        private PowerStation stationModel = new PowerStation();
+
+        internal PowerStation StationModel
+        {
+            get
+            {
+                return stationModel;
+            }            
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            StationModelChanged();
+        }
+
+        void StationModelChanged()
+        {
+            List<PowerItem> items = StationModel.ItemsByVoltage(0);
+            textFilteredItems.Document.Blocks.Clear();
+            
+            foreach (PowerItem item in items)
+            {
+                textFilteredItems.AppendText("\n" + item.ToString());
+            }
+            double sum = StationModel.GetSum().Magnitude;
+            labelSumTotal.Content = "S = " + sum.ToString("#.0");
+        }
+
+        private void menuExit_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Do you want to exit application?", "Power Calc APP", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (result == MessageBoxResult.Yes)
+            {
+                Close();
+            }
+        }
+
+        private void itemAddLoad_Click(object sender, RoutedEventArgs e)
+        {
+
+            NewItemAddDlg dlg = new NewItemAddDlg();
+            dlg.Title = "Add Load";
+            Nullable<bool> dialogResult = dlg.ShowDialog();
+            if (dialogResult.Value)
+            {
+                StationModel.Items.Add(new PowerLoad() { Vnom = dlg.Unom, Pnom = dlg.Snom });
+                StationModelChanged();
+            }
+        }
+
+        private void itemAddCapacitor_Click(object sender, RoutedEventArgs e)
+        {
+            NewItemAddDlg dlg = new NewItemAddDlg();
+            dlg.Title = "Add Capacitor";
+            Nullable<bool> dialogResult = dlg.ShowDialog();
+            if (dialogResult.Value)
+            {
+                StationModel.Items.Add(new PowerCapacitor() { Vnom = dlg.Unom, Qnom = dlg.Snom });
+                StationModelChanged();
+            }
+        }
+
+        private void textFilteredItems_TextChanged(object sender, TextChangedEventArgs e)
+        {
+        }
+
+        private void menuFileNew_Click(object sender, RoutedEventArgs e)
+        {
+            stationModel = new PowerStation();
+            StationModelChanged();
+        }
+
+        private void menuFileOpen_Click(object sender, RoutedEventArgs e)
+        {
+            stationModel = PowerStation.LoadFromXML("Data.xml");
+            StationModelChanged();
+        }
+
+        private void menuFileSave_Click(object sender, RoutedEventArgs e)
+        {
+            stationModel.SaveToXML("Data.xml");
+        }
+    }
+}
